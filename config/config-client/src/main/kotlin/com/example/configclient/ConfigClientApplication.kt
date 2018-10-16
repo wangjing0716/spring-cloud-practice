e7e84621b14de9a0591e337465462e8038c2b93a
wangjing <jinnywang@fintend.com>
2018-10-16 16:20:37 +0800

    config-client加bus

--- config/config-client/src/main/kotlin/com/example/configclient/ConfigClientApplication.kt
+++ config/config-client/src/main/kotlin/com/example/configclient/ConfigClientApplication.kt
@@ -2,8 +2,14 @@ package com.example.configclient
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 class ConfigClientApplication
 
 fun main(args: Array<String>) {
