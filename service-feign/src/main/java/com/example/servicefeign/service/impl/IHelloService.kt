cfb3f140078b8dd4cb4b97a3f614a28aacd286e0
wangjing <jinnywang@fintend.com>
2018-10-16 10:01:16 +0800

    Feign中使用断路器

--- service-feign/src/main/java/com/example/servicefeign/service/impl/IHelloService.kt
+++ service-feign/src/main/java/com/example/servicefeign/service/impl/IHelloService.kt
@@ -1,10 +1,11 @@
 package com.example.servicefeign.service.impl
 
+import com.example.servicefeign.service.HelloService
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 
-@FeignClient(value="eureka-client")
+@FeignClient(value="eureka-client", fallback = HelloService::class)
 interface IHelloService {
 
     @GetMapping(value = "/hi")
