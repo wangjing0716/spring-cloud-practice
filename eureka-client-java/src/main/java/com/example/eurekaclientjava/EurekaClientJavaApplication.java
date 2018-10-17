d7ce7da1b509b0a153e2280819bfe792d0d10536
wangjing <jinnywang@fintend.com>
2018-10-17 10:16:30 +0800

    hystrix-dashboard

---
+++ eureka-client-java/src/main/java/com/example/eurekaclientjava/EurekaClientJavaApplication.java
@@ -0,0 +1,22 @@
+package com.example.eurekaclientjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+public class EurekaClientJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientJavaApplication.class, args);
+    }
+}
